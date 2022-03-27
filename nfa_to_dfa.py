c9c603d3c589bf53fe51ccc58391f4f774a4424c
Nathanael Gastelum <nathanaelgastelum@gmail.com>
2022-03-27 14:57:29 -0700

    implemented powerset and output formatting

--- nfa_to_dfa.py
+++ nfa_to_dfa.py
@@ -13,30 +13,55 @@
 # E(4) = {4}
 
 from collections import deque
+from platform import node
 
-
+# TODO: read input from file 
 nfa = {1: {2, 3}, 
         2: "empty",
         3: {4},
         4: "empty"}
 
-dfa = {}
 
-for key in nfa.keys():
-    dfa[key] = {key}
+def get_powerset(nfa):
+    node_set = list(nfa)
+    powerset = []
 
-    if nfa[key] != "empty":
-        nodes = deque()
-        nodes.append(nfa[key])
+    for i in range(1 << len(node_set)):
+        powerset.append([])
+        for j in range(len(node_set)):
+            if i & (1 << j):
+                powerset[i].append(node_set[j])
 
-        while nodes:
-            node = nodes.popleft()
+    return powerset
 
-            # Conversion happens here
-            if node not in dfa[key] and node != "empty": 
-                dfa[key].update(node)
-                
-                for i in node:
-                    nodes.append(nfa[i])
 
-print(dfa)
+def nfa_to_dfa(nfa):
+    dfa = {}
+
+    for key in nfa.keys():
+        dfa[key] = {key}
+
+        if nfa[key] != "empty":
+            nodes = deque()
+            nodes.append(nfa[key])
+
+            while nodes:
+                node = nodes.popleft()
+
+                # Conversion happens here
+                if node not in dfa[key] and node != "empty": 
+                    dfa[key].update(node)
+                    
+                    for i in node:
+                        nodes.append(nfa[i])
+
+    return dfa
+
+
+def print_output(nfa):
+    print(sorted(get_powerset(nfa), key=len))
+
+    for key, values in nfa_to_dfa(nfa).items():
+        print(f"E({key}) = {values}")
+
+print_output(nfa)
